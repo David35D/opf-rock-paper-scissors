6fe00c410493009f979c2bb98e007c3689085952
David Delgado <david.delgadop88@gmail.com>
2022-12-31 21:25:29 -0400

    added missing semi colon, strict mode declaration

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+'use strict';
+
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     const choice = Math.floor(Math.random() * 2);
-    return choices[choice]
+    return choices[choice];
 }
